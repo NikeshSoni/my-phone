2728f1eacce2e711e515999a5ca2b809ad833715
NikeshSoni <nikesh21soni@gmail.com>
2023-01-16 20:30:11 +0530

    just tray new
    just tray new

---
+++ js/header.js
@@ -0,0 +1,31 @@
+
+
+
+   //  //  const principleAmt = 
+     
+   //  //  document.querySelector('.Principle-amt').addEventListener('click', function (main) {
+
+   //  //         console.log();
+        
+   //  //  })
+    
+
+   //  //  console.log( ' nikki' ,principleAmt);
+
+   //  document.querySelector('.click-btn').addEventListener('click', function calculate() {
+
+   //      // const principle_amt = 0;
+   //      // const rate_amt = 0;
+   //      // const year_amt = 0;
+   //      // const interest_amt = 0;
+
+   //   let   principle = document.querySelector('.Principle-amt').value;
+   //   let   rate = document.querySelector('.rate-interest').value;
+   //   let   year = document.querySelector('.rate-month').value;
+   //   let   interest =  (principle*year*rate)/100;
+
+   //      document.querySelector('.income').innerHTML = interest;
+
+
+   //      console.log(principle);
+   //   })
